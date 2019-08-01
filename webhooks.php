1f5b43e894a16d3fcaf536d32cc5a1c2ab278fdd
dracee <42826410+dracee@users.noreply.github.com>
2019-08-01 23:40:50 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'c6/kuui/k7WLWa5EJ9OdUguu5Y7mdWSI1YwHbgIEXgqp4Qavf0QmXTuR0+T3NLkO0QTmjxUVFkDbAxprS75kbLax3RUXRRglpKtoouvBWG7T0vtz67YZ7Nwq3ig66fpSqglcasgZGYKSQGGDLBuIg1GUYhWQfeY8sLGRXgo3xvw=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
